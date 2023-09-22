dc3e77db2ec3fba6b8a8a0b38e6156840a061a9c
osman resat dogrul <osmanresatdogrul@gmail.com>
2023-09-22 20:32:07 +0300

    Initial Commit

---
+++ CompanyChat/CompanyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanyChatApp.swift
+//  CompanyChat
+//
+//  Created by Osman Reşat Doğrul on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CompanyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
